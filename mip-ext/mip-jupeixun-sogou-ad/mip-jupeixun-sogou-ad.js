1a63372f74586631a7f81e520b5ebbacf78f6835
libaoan <287639598@qq.cm>
2018-07-12 22:11:55 +0800

    html

--- mip-ext/mip-jupeixun-sogou-ad/mip-jupeixun-sogou-ad.js
+++ mip-ext/mip-jupeixun-sogou-ad/mip-jupeixun-sogou-ad.js
@@ -5,15 +5,16 @@
  */
 
 define(function (require) {
-    'use strict';
+    //'use strict';
 
-    var customElement = require('customElement').create();
     var $ = require('zepto');
+    var customElement = require('customElement').create();
+    
 
     /**
      * 第一次进入可视区回调，只会执行一次
      */
-    customElement.prototype.firstInviewCallback = function () {
+    customElement.prototype.createdCallback = function () {
         // TODO
         var ele = this.element;
         var adWidth = ele.getAttribute('ad_width');
@@ -21,16 +22,30 @@ define(function (require) {
         var adId = ele.getAttribute('ad_id');
         var adSrc = ele.getAttribute('ad_src');
 
+        var scriptTag1 = document.createElement('script');
+        scriptTag1.type = 'text/javascript';
+
         var adhtml = 'var sogou_ad_id=' + adId + ';';
         adhtml += 'var sogou_ad_content_height=' + adHeight + ';';
-        var scriptTag1 = document.createElement('script');
         scriptTag1.innerHTML = adhtml;
 
         var scriptTag2 = document.createElement('script');
-        scriptTag2.src = adSrc,
+        scriptTag2.type = 'text/javascript';
+        scriptTag2.src = 'https://theta.sogoucdn.com/wap/js/wp.js';
 
         ele.appendChild(scriptTag1);
         ele.appendChild(scriptTag2);
+
+        // var adhtml = 'var sogou_ad_id=' + adId + ';';
+        // adhtml += 'var sogou_ad_content_height=' + adHeight + ';';
+        // var scriptTag1 = document.createElement('script');
+        // scriptTag1.innerHTML = adhtml;
+
+        // var scriptTag2 = document.createElement('script');
+        // scriptTag2.src = adSrc,
+
+        // ele.appendChild(scriptTag1);
+        // ele.appendChild(scriptTag2);
     };
 
     return customElement;
